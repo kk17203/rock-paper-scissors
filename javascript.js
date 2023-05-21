010cc2b8e75a53c232ba351af025953208c406d1
kk17203 <kk17203@gmail.com>
2023-05-20 21:30:08 -0500

    Working on linking buttons to function.

--- javascript.js
+++ javascript.js
@@ -11,47 +11,46 @@ for (let i = 0; i = playerScoreCount < 5 && computerScoreCount < 5; i++) {
 // randomly choose rock, paper, or scissors
 let compChoice = ['Rock', 'Paper', 'Scissors'];
 
-let getComputerChoice = compChoice[Math.floor(Math.random() * 3)];
+let getComputerSelection = compChoice[Math.floor(Math.random() * 3)];
 
 // convert getComputerChoice to uppercase (to make case insensitive)
-let computerSelection = getComputerChoice.toUpperCase();
+//let computerSelection = getComputerChoice.toUpperCase();
 
 // get user selection
 //let getPlayerChoice = prompt('Choose "Rock", "Paper", or "Scissors"', '');
 //function playerChoice (getPlayerChoice) {
 const btn = document.querySelectorAll('.btn');
-let getPlayerChoice = btn.forEach(btn => btn.addEventListener('click', function(e) {return(e.target.innerText);}));
+let playerSelection = btn.forEach(btn => btn.addEventListener('click', function(e) {return(e.target.innerText);}));
 
 
 
 
-
- console.log(getPlayerChoice);
+ console.log(playerSelection);
 
 // convert getPlayerChoice to uppercase (to make case insensitive)
-let playerSelection = getPlayerChoice.toUpperCase();
+//let playerSelection = getPlayerChoice.toUpperCase();
 
 // function that compares selections and declares winner
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) {
+function playRound(playerSelection, getComputerSelection) {
+    if (playerSelection == getComputerSelection) {
         return ('Tie!');
-    } else if (playerSelection == 'ROCK' && computerSelection == 'PAPER') {
+    } else if (playerSelection == 'Rock' && getComputerSelection == 'Paper') {
         return ('You Lose! Paper beats rock!');
-    } else if (playerSelection == 'SCISSORS' && computerSelection == 'PAPER') {
+    } else if (playerSelection == 'Scissors' && getComputerSelection == 'Paper') {
         return ('You Win! Scissors beat paper!');
-    } else if (playerSelection == 'PAPER' && computerSelection == 'SCISSORS') {
+    } else if (playerSelection == 'Paper' && getComputerSelection == 'Scissors') {
         return ('You Lose! Scissors beat paper!');
-    } else if (playerSelection == 'ROCK' && computerSelection == 'SCISSORS') {
+    } else if (playerSelection == 'Rock' && getComputerSelection == 'Scissors') {
         return ('You Win! Rock beats scissors!');
-    } else if (playerSelection == 'SCISSORS' && computerSelection == 'ROCK') {
+    } else if (playerSelection == 'Scissors' && getComputerSelection == 'Rock') {
         return ('You Lose! Rock beats scissors!');
-    } else if (playerSelection == 'PAPER' && computerSelection == 'ROCK') {
+    } else if (playerSelection == 'Paper' && getComputerSelection == 'Rock') {
         return ('You Win! Paper beats Rock!');
     }
     }
 
 // display round winner
-console.log(playRound(playerSelection, computerSelection));
+console.log(playRound(playerSelection, getComputerSelection));
 
 // keep track of overall score
 if (playRound(playerSelection, computerSelection).includes('You Win!')) {
