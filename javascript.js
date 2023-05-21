b57d8a0672aa229729cd52df33f5c2d559c794c6
kk17203 <kk17203@gmail.com>
2023-05-21 08:04:30 -0500

    Revert back to main branch. Add event listener.

--- javascript.js
+++ javascript.js
@@ -6,57 +6,56 @@ let playerScoreCount = 0;
 let computerScoreCount = 0;
 
 // loop until player or comp gets 5 wins
-//for (let i = 0; i = playerScoreCount < 5 && computerScoreCount < 5; i++) {
+for (let i = 0; i = playerScoreCount < 5 && computerScoreCount < 5; i++) {
 
 // randomly choose rock, paper, or scissors
 let compChoice = ['Rock', 'Paper', 'Scissors'];
 
-let getComputerSelection = compChoice[Math.floor(Math.random() * 3)];
+let getComputerChoice = compChoice[Math.floor(Math.random() * 3)];
 
 // convert getComputerChoice to uppercase (to make case insensitive)
-//let computerSelection = getComputerChoice.toUpperCase();
+let computerSelection = getComputerChoice.toUpperCase();
 
 // get user selection
 //let getPlayerChoice = prompt('Choose "Rock", "Paper", or "Scissors"', '');
-//function playerChoice (getPlayerChoice) {
-const rock = document.querySelector('.rock');
-rock.addEventListener('click', () => {
-    const playerSelection = 'Rock';
-    playRound ();
-    console.log(playerSelection);
-});
+const btn = document.querySelectorAll('.btn');
 
- 
+btn.forEach(btn =>
+    btn.addEventListener('click', function (e) {
+        console.log(e.target.innerText)
+    }));
+
+// console.log(getPlayerChoice);
 
 // convert getPlayerChoice to uppercase (to make case insensitive)
-//let playerSelection = getPlayerChoice.toUpperCase();
+let playerSelection = getPlayerChoice.toUpperCase();
 
 // function that compares selections and declares winner
-function playRound(playerSelection, getComputerSelection) {
-    if (playerSelection == getComputerSelection) {
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
         return ('Tie!');
-    } else if (playerSelection == 'Rock' && getComputerSelection == 'Paper') {
+    } else if (playerSelection == 'ROCK' && computerSelection == 'PAPER') {
         return ('You Lose! Paper beats rock!');
-    } else if (playerSelection == 'Scissors' && getComputerSelection == 'Paper') {
+    } else if (playerSelection == 'SCISSORS' && computerSelection == 'PAPER') {
         return ('You Win! Scissors beat paper!');
-    } else if (playerSelection == 'Paper' && getComputerSelection == 'Scissors') {
+    } else if (playerSelection == 'PAPER' && computerSelection == 'SCISSORS') {
         return ('You Lose! Scissors beat paper!');
-    } else if (playerSelection == 'Rock' && getComputerSelection == 'Scissors') {
+    } else if (playerSelection == 'ROCK' && computerSelection == 'SCISSORS') {
         return ('You Win! Rock beats scissors!');
-    } else if (playerSelection == 'Scissors' && getComputerSelection == 'Rock') {
+    } else if (playerSelection == 'SCISSORS' && computerSelection == 'ROCK') {
         return ('You Lose! Rock beats scissors!');
-    } else if (playerSelection == 'Paper' && getComputerSelection == 'Rock') {
+    } else if (playerSelection == 'PAPER' && computerSelection == 'ROCK') {
         return ('You Win! Paper beats Rock!');
     }
     }
 
 // display round winner
-console.log(playRound(playerSelection, getComputerSelection));
+console.log(playRound(playerSelection, computerSelection));
 
 // keep track of overall score
-if (playRound(playerSelection, getComputerSelection).includes('You Win!')) {
+if (playRound(playerSelection, computerSelection).includes('You Win!')) {
     playerScoreCount++
-} else if (playRound(playerSelection, getComputerSelection).includes('You Lose!')) {
+} else if (playRound(playerSelection, computerSelection).includes('You Lose!')) {
     computerScoreCount++
 }
 
@@ -64,15 +63,14 @@ if (playRound(playerSelection, getComputerSelection).includes('You Win!')) {
 console.log('Your Score: ' + playerScoreCount);
 console.log('Computer Score: ' + computerScoreCount);
  
-//}
+}
 
 // display overall winner
-//if (playerScoreCount > computerScoreCount) {
-//    alert ('Congratulations! You Won!');
-//} else if (playerScoreCount < computerScoreCount) {
-//    alert ('Bummer! You Lost!');
-//} else {
-//    alert ("Unbelievable! It's a Tie!");
-//}
-
+if (playerScoreCount > computerScoreCount) {
+    alert ('Congratulations! You Won!');
+} else if (playerScoreCount < computerScoreCount) {
+    alert ('Bummer! You Lost!');
+} else {
+    alert ("Unbelievable! It's a Tie!");
+}
 
